21b893f272c75160df7d662d7965a04ed6ec4ccf
als21021 <als21021@naver.com>
2026-04-21 17:28:43 +0900

    day1Minus

---
+++ Programmers/Day1/minus/main.cpp
@@ -0,0 +1,22 @@
+/*
+문제 설명
+정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = num1 - num2;
+    return answer;
+}
+
+int main() {
+    int num1, num2;
+    cin >> num1 >> num2;
+
+    cout << solution;
+}
