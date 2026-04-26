d7901f748290eb29c858f8ee6b89bcb38498e0c3
als21021 <als21021@naver.com>
2026-04-26 22:36:24 +0900

    day6repeatChar

---
+++ Programmers/Day6/repeatChar/solution.cpp
@@ -0,0 +1,33 @@
+/*
+문제 설명
+문자열 my_string과 정수 n이 매개변수로 주어질 때, my_string에 들어있는 각 문자를 n만큼 반복한 문자열을 return 하도록 solution 함수를 완성해보세요.
+*/
+
+/*
+제한사항
+2 ≤ my_string 길이 ≤ 5
+2 ≤ n ≤ 10
+"my_string"은 영어 대소문자로 이루어져 있습니다.
+*/
+
+/*
+풀이
+이것도 이중 반복문 하면 될 거 같은데...
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int n) {
+    string answer = "";
+
+    for(int i = 0; i < my_string.size(); i++) {
+        for(int j = 0; j < n; j++) {
+            answer += my_string[i];
+        }
+
+    }
+    return answer;
+}
