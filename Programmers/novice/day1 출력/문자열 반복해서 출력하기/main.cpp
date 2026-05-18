613701859222e04f253ec53ec03eea7564701bbb
als21021 <als21021@naver.com>
2026-05-18 14:09:31 +0900

    novice/day1/문자열 반복해서 출력하기

---
+++ Programmers/novice/day1 출력/문자열 반복해서 출력하기/main.cpp
@@ -0,0 +1,27 @@
+/*
+문제 설명
+문자열 str과 정수 n이 주어집니다.
+str이 n번 반복된 문자열을 만들어 출력하는 코드를 작성해 보세요.
+*/
+
+/*
+제한사항
+1 ≤ str의 길이 ≤ 10
+1 ≤ n ≤ 5
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    int n;
+    cin >> str >> n;
+
+    for(int i = 0; i < n; i++) {
+        cout << str;
+    }
+    return 0;
+}
