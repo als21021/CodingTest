5f270bdcfb8ca5e27cd5f4f57e07ca088004837e
als21021 <als21021@naver.com>
2026-05-29 21:57:28 +0900

    novice/day4/flag에 따라 다른 값 반환하기

---
+++ Programmers/02_novice/Day04 연산, 조건문/flag에 따라 다른 값 반환하기/solution.cpp
@@ -0,0 +1,21 @@
+/*
+문제 설명
+두 정수 a, b와 boolean 변수 flag가 매개변수로 주어질 때,
+flag가 true면 a + b를 false면 a - b를 return 하는 solution 함수를 작성해 주세요.
+*/
+
+/*
+제한사항
+-1,000 ≤ a, b ≤ 1,000
+*/
+
+/*
+풀이
+flag? 로 넣어놓고
+하면 될 듯
+*/
+
+int solution(int a, int b, bool flag) {
+
+    return flag ? a + b : a - b;
+}
