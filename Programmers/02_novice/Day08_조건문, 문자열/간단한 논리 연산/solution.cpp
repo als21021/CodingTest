0975965f29370ac03ee8b70fb6d856a492124cd2
als21021 <als21021@naver.com>
2026-06-02 13:37:40 +0900

    novice/day8/간단한 논리 연산

---
+++ Programmers/02_novice/Day08_조건문, 문자열/간단한 논리 연산/solution.cpp
@@ -0,0 +1,18 @@
+/*
+문제 설명
+boolean 변수 x1, x2, x3, x4가 매개변수로 주어질 때,
+다음의 식의 true/false를 return 하는 solution 함수를 작성해 주세요.
+
+(x1 ∨ x2) ∧ (x3 ∨ x4)
+*/
+
+/*
+풀이
+v는 or
+^는 and
+*/
+
+bool solution(bool x1, bool x2, bool x3, bool x4) {
+
+    return (x1 || x2) && (x3 || x4);
+}
