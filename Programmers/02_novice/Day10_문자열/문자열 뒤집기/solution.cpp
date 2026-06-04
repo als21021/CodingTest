36d9e36b639576b2a6706c1e796037b231b306ba
als21021 <als21021@naver.com>
2026-06-04 11:41:55 +0900

    novice/day10/문자열 뒤집기

---
+++ Programmers/02_novice/Day10_문자열/문자열 뒤집기/solution.cpp
@@ -0,0 +1,27 @@
+/*
+문제 설명
+문자열 my_string과 정수 s, e가 매개변수로 주어질 때,
+my_string에서 인덱스 s부터 인덱스 e까지를 뒤집은 문자열을 return 하는 solution 함수를 작성해 주세요.
+*/
+
+/*
+제한사항
+my_string은 숫자와 알파벳으로만 이루어져 있습니다.
+1 ≤ my_string의 길이 ≤ 1,000
+0 ≤ s ≤ e < my_string의 길이
+*/
+
+/*
+풀이
+reverse(s, e)
+*/
+
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string solution(string my_string, int s, int e) {
+
+    reverse(my_string.begin() + s, my_string.begin() + e + 1);
+    return my_string;
+}
