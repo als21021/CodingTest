95aa6c0787a00d0928af273c9b1547ff156a84af
als21021 <als21021@naver.com>
2026-06-07 09:31:47 +0900

    novice/day13/n번째 원소부터

---
+++ Programmers/02_novice/Day13_리스트/n번째 원소부터/solution.cpp
@@ -0,0 +1,27 @@
+/*
+문제 설명
+
+정수 리스트 num_list와 정수 n이 주어질 때, n 번째 원소부터 마지막 원소까지의 모든 원소를 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+*/
+
+/*
+제한사항
+2 ≤ num_list의 길이 ≤ 30
+1 ≤ num_list의 원소 ≤ 9
+1 ≤ n ≤ num_list의 길이
+*/
+
+/*
+풀이
+
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+    vector<int> a(num_list.begin() + n - 1, num_list.end());
+    return a;
+}
