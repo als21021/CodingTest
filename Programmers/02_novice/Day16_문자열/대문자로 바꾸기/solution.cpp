0e3458fc66653abc90f4d99d3de31a3b7e83876b
als21021 <als21021@naver.com>
2026-06-10 13:58:10 +0900

    novice/day16/대문자로 바꾸기

---
+++ Programmers/02_novice/Day16_문자열/대문자로 바꾸기/solution.cpp
@@ -0,0 +1,18 @@
+/*
+문제 설명
+
+알파벳으로 이루어진 문자열 myString이 주어집니다.
+모든 알파벳을 대문자로 변환하여 return 하는 solution 함수를 완성해 주세요.
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    for(char& c : myString) {
+        c = toupper(c);
+    }
+    return myString;
+}
