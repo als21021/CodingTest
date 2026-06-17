dbc92fe3e80ccfde94bb7e22cab77066883ac3c5
als21021 <als21021@naver.com>
2026-06-17 16:34:02 +0900

    novice/day22/문자열로 변환

---
+++ Programmers/02_novice/Day22_함수, 조건문/문자열로 변환/solution.cpp
@@ -0,0 +1,19 @@
+/*
+문제 설명
+
+정수 n이 주어질 때,
+n을 문자열로 변환하여 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+1 ≤ n ≤ 10000
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = to_string(n);
+    return answer;
+}
