ebc83092df3980dabb877237d2d235a0c8530622
als21021 <als21021@naver.com>
2026-06-18 13:13:30 +0900

    novice/day23/정수 찾기

---
+++ Programmers/02_novice/Day23_조건문 활용/정수 찾기/solution.cpp
@@ -0,0 +1,23 @@
+/*
+문제 설명
+
+정수 리스트 num_list와 찾으려는 정수 n이 주어질 때,
+num_list안에 n이 있으면 1을 없으면 0을 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+3 ≤ num_list의 길이 ≤ 100
+1 ≤ num_list의 원소 ≤ 100
+1 ≤ n ≤ 100
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list, int n) {
+    for(int num : num_list) {
+        if(num == n) return 1;
+    }
+    return 0;
+}
