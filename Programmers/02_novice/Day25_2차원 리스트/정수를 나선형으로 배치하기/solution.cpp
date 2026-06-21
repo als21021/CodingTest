82a1ee84078d5426941c2f18721e0111d8275981
als21021 <als21021@naver.com>
2026-06-21 20:45:48 +0900

    novice/day25/특별한 2차원 배열 2

--- Programmers/02_novice/Day25_2차원 리스트/정수를 나선형으로 배치하기/solution.cpp
+++ Programmers/02_novice/Day25_2차원 리스트/정수를 나선형으로 배치하기/solution.cpp
@@ -1,41 +1,35 @@
 /*
 문제 설명
 
-양의 정수 n이 매개변수로 주어집니다.
-n × n 배열에 1부터 n2 까지 정수를 인덱스 [0][0]부터 시계방향 나선형으로 배치한 이차원 배열을 return 하는 solution 함수를 작성해 주세요.
+n × n 크기의 이차원 배열 arr이 매개변수로 주어질 때, arr이 다음을 만족하면 1을 아니라면 0을 return 하는 solution 함수를 작성해 주세요.
+0 ≤ i, j < n인 정수 i, j에 대하여 arr[i][j] = arr[j][i]
 
 제한사항
-1 ≤ n ≤ 30
+1 ≤ arr의 길이 = arr의 원소의 길이 ≤ 100
+1 ≤ arr의 원소의 원소 ≤ 1,000
+모든 arr의 원소의 길이는 같습니다.
 */
 
 /*
 풀이
-0까지 x값 감소, n까지 y값 증가, n까지 x값 증가, 1까지 y값 감소
+arr[i][j] == arr[j][i]
 
-1까지 x값 감소, n-1까지 y값 증가, n-1까지 x값 증가, 1까지 y값 감소
-
-.
-.
-.
-
-끝나는 시점 잡아야 하는데
-짝수면 n/2 까지
-홀수면 n/2 까지
+이중반복문 써서 저 식 그대로 넣고 false면 0 return
+반복문 끝까지 return 안되면 1 return
 */
 
 #include <vector>
 using namespace std;
 
-vector<vector<int>> solution(int n) {
+int solution(vector<vector<int>> arr) {
 
-    vector<vector<int>> answer(n, vector<int> (n));
-    int value = 1;
+    int n = arr.size();
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
 
-    for(int i = 0; i < n / 2; i++) {
-
-        for(int j = i; j < n - i; j++) answer[i][j] = value++;
-
-        for(int j = i + 1; j < n - i; j++) answer[j][n-1-i] = value++;
+            if(arr[i][j] != arr[j][i]) return 0;
+        }
     }
-    return answer;
+
+    return 1;
 }
