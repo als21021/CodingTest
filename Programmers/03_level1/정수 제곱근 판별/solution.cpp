47edcee7028bd4d838e60690c1e3b1ce4a76f955
als21021 <als21021@naver.com>
2026-08-07 13:53:38 +0900

    level1/정수 제곱근 판별

---
+++ Programmers/03_level1/정수 제곱근 판별/solution.cpp
@@ -0,0 +1,24 @@
+/*
+정수 제곱근 판별
+
+문제 설명
+
+임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
+n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
+제한 사항
+n은 1이상, 50000000000000 이하인 양의 정수입니다.
+*/
+
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    double doubleNum = sqrt(n);
+    int intNum = sqrt(n);
+    if(doubleNum == intNum) return pow(doubleNum + 1, 2);
+    else return -1;
+}
